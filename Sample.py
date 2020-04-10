aded3e645774e594374fcd3f4053e70bacade348
Logan Tarvit <Zer0thenumber@hotmail.com>
2020-04-10 16:09:26 +0100

    Create Sample.py

---
+++ Sample.py
@@ -0,0 +1,48 @@
+
+def COMMAND(PINGSTAGE, TIME): # Function to send control command.
+	
+	if (time != 0): # If Time in seconds is not 0.
+		
+		COSMIC2.Serial(COMMAND + ".RESPOND," + TIME + "."); # Send command with time attached.
+		
+		time.sleep(1);
+
+		receive ACK
+			
+			PINGSTAGE == ("ACK");
+			
+			return;
+	
+	else: # If Time is not specified. ( Most of the time. )
+		
+		COSMIC2.Serial(COMMAND + ".RESPOND,ASAP."); # Send Command with standard instant response.
+		
+		time.sleep(1);
+			
+			PINGSTAGE == ("ACK");
+			
+			return;
+			
+def DATA(): # Function to retrieve data.
+	
+	request (TEMP, LIGHT, HUMIDITY, FOREDISTANCE, SIDEDISTANCE);
+	
+	time.sleep(1);
+	
+	recieve (TEMP, LIGHT, HUMIDITY, FOREDISTANCE, SIDEDISTANCE);
+	
+	time.sleep(1);
+	
+	receive ACK
+
+	return (TEMP, LIGHT, HUMIDITY, FOREDISTANCE, SIDEDISTANCE);
+
+def CONTROL(CONTROL, VALUE, TIME):
+
+	COSMIC2.Serial(CONTROL, VALUE, TIME);
+
+	time.sleep(1);
+
+	receive ACK
+
+	return (ACK) # Function to send movement Data.
